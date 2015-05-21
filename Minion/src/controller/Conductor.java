132b444d42e06f6e5b31154b712f42d142f621cd
Daniel <ddger@web.de>
2015-05-21 19:18:19 +0200

    changed to Object of Conductor.

--- Minion/src/controller/Conductor.java
+++ Minion/src/controller/Conductor.java
@@ -8,24 +8,29 @@ import java.util.ArrayList;
 
 public class Conductor {
 	ArrayList<FastAEntry> correctfastAList;
+	ArrayList<IncorrectFastAEntry> incorrectfastAList;
+	
 
 	/*
 	 * called by the ActionListener of the gui. gets the filename and and calles the model functions.
 	 * Returns the a ArrayList of incorrectFatAFiles.
 	 */
-	public ArrayList<IncorrectFastAEntry> modelFile(String filename) throws IOException
-	{
-		ParseFastA newParsedfile = new ParseFastA(filename);
-		Conductor conductor = new Conductor(newParsedfile.getFastAList());
-		
-		
-		return newParsedfile.getFastAErrorList();	//returns the ArrayList of incorrect FastA Files
-	}
+
 	/*
 	 * gets the ArrayList of correct FastaEntrys. Stores it in the Conductor Object.
 	 */
-	private Conductor(ArrayList<FastAEntry> correctfastAList)
+	private Conductor(String filename) throws IOException
 	{
-		correctfastAList = correctfastAList;
+		ParseFastA newParsedfile = new ParseFastA(filename);
+		this.correctfastAList = newParsedfile.getFastAList();
+		this.incorrectfastAList = newParsedfile.getFastAErrorList();
+	}
+	public ArrayList<FastAEntry> getcorrectfastAList()
+	{
+		return this.correctfastAList;
+	}
+	public ArrayList<IncorrectFastAEntry> getincorrectfastAList()
+	{
+		return this.incorrectfastAList;
 	}
 }
