d4acfb7b78758d2a054c2cd41328b03d92c98201
FriederikeHanssen <friederike.hanssen@student.uni-tuebingen.de>
2015-06-03 15:50:27 -0700

    Basecalling error class w\o values

--- Programmierprojekt/FastA/src/error/ErrorBasecalling.java
+++ Programmierprojekt/FastA/src/error/ErrorBasecalling.java
@@ -14,10 +14,10 @@ public class ErrorBasecalling implements ErrorModel{
 	@Override
 	public String apply(String seq, int basecalling) {
 		// TODO Auto-generated method stub
-		ErrorRate rate = new ErrorRate();
+		ErrorRate rate = new ErrorRate(basecalling);
 		String err = "";
 		for(int i = 0; i < seq.length();i++){
-			err.concat(mutate(seq.charAt(i),basecalling,rate));
+			err.concat(mutate(seq.charAt(i),rate));
 		}
 		return err;
 	}
@@ -25,9 +25,21 @@ public class ErrorBasecalling implements ErrorModel{
 	
 	
 	
-	private String mutate(char letter,int basecalling, ErrorRate rate){
-		Chance rand = new Chance();
+	private String mutate(char letter, ErrorRate rate){
 		
+		Chance rand = new Chance();
+		double prob = rand.getRand();
+		int row = rate.getRow(letter);
+		
+		if(prob <= rate.getValue(row, 0)){
+			return rate.getBase(0);
+		}else if(prob > rate.getValue(row, 0) && prob <= rate.getValue(row, 1)){
+			return rate.getBase(1);
+		}else if(prob > rate.getValue(row, 1) && prob <= rate.getValue(row, 2)){
+			return rate.getBase(2);
+		}else if(prob > rate.getValue(row, 2) && prob <= rate.getValue(row, 3)){
+			return rate.getBase(3);
+		}
 		return "";
 	}
 	
--- Programmierprojekt/FastA/src/error/ErrorRate.java
+++ Programmierprojekt/FastA/src/error/ErrorRate.java
@@ -1,13 +1,79 @@
 package error;
 
+/**
+ * 
+ * @author Friederike Hanssen
+ *
+ */
 public class ErrorRate {
 
 
-	public ErrorRate(){
+	private double[][] transProb;
+	private char[] base = {'A','C','T','G'};
+	public ErrorRate(int basecalling){
 		
-		double[][] transProb1D = {{},{},{},{}};
-		double[][] transProb2D = {{},{},{},{}};
+		
+		this.transProb = new double[4][4];
+		generate(basecalling);
+		
 	}
+	
+	private void generate(int basecalling){
+		
+			if(basecalling == 1){
+
+				transProb[0][0] = 1;
+				transProb[0][1] = 1;
+				transProb[0][2] = 1;
+				transProb[0][3] = 1;
+				transProb[1][0] = 1;
+				transProb[1][1] = 1;
+				transProb[1][2] = 1;
+				transProb[1][3] = 1;
+				transProb[2][0] = 1;
+				transProb[2][1] = 1;
+				transProb[2][1] = 1;
+				transProb[2][3] = 1;
+				transProb[3][0] = 1;
+				transProb[3][1] = 1;
+				transProb[3][2] = 1;
+				transProb[3][3] = 1;
+			}else if(basecalling == 2){
+				transProb[0][0] = 1;
+				transProb[0][1] = 1;
+				transProb[0][2] = 1;
+				transProb[0][3] = 1;
+				transProb[1][0] = 1;
+				transProb[1][1] = 1;
+				transProb[1][2] = 1;
+				transProb[1][3] = 1;
+				transProb[2][0] = 1;
+				transProb[2][1] = 1;
+				transProb[2][1] = 1;
+				transProb[2][3] = 1;
+				transProb[3][0] = 1;
+				transProb[3][1] = 1;
+				transProb[3][2] = 1;
+				transProb[3][3] = 1;
+			}
+	}
+	
+	public double getValue(int i, int j){
+		return transProb[i][j];
+	}
+	
+	public String getBase(int i){
+		return Character.toString(base[i]);
+	}
+	
+	public int getRow(char a){
+		for(int i = 0; i < base.length;i++){
+			if(a == base[i])
+				return i;
+		}
+		return -1;
+	}
+	
 	
 	
 }
