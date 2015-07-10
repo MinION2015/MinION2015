7477e8964281b101e4bf24e14f43bcd31ef3e2d3
FriederikeHanssen <friederike.hanssen@student.uni-tuebingen.de>
2015-07-09 17:19:59 -0700

    finished implementing fastQ reader

--- Minion/src/reader/FastQ.java
+++ Minion/src/reader/FastQ.java
@@ -1,21 +1,33 @@
 package reader;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import error.ErrorCodes;
 import error.MyException;
 
+/**
+ * 
+ * @author Friederike Hanssen
+ * @functionality A FastQ object can parse a file or add existing sequences to itself. It implements the FiletypeContaing sequences interface.
+ * A fastq object is assumed to have conventional format without line breaks. According to wikipedia it consists of 4 lines: header, sequence, metadata,score, with sequence and score being of the same length
+ * 
+ * @input It doesn't have an input. FastQ contains two array lists of the same length. One stores the entry in the fastq file the other some kind of error information.
+ * @output outputs the data in an output file
+ */
 public class FastQ implements FiletypeContainingSequences{
 
-	private ArrayList<FastQSequence> seqList;
+	private ArrayList<Sequence> seqList;
 	private ArrayList<MyException> errList;
 	
 	public FastQ(){
-		this.seqList = new ArrayList<FastQSequence>;
+		this.seqList = new ArrayList<Sequence>();
 		this.errList = new ArrayList<MyException>();
 	}
 	public void parse(String inputFilename) throws IOException, MyException {
@@ -67,115 +79,126 @@ public class FastQ implements FiletypeContainingSequences{
 		
 		try{
 			checkForReadingError(header,sequence,addInfo,score);
-			FastQSequence seq = new Sequence(header, sequence,addInfo,score);
+			Sequence seq = new FastQSequence(header, sequence,addInfo,score);
 			MyException err = new MyException(ErrorCodes.NO_ERROR);
 			seqList.add(seq);
 			errList.add(err);
 		}catch(MyException e){
-			if(e.getErrorCode() == 2003){//gapped seq
-				FastQSequence seq = new FastQSequence(header, sequence,addInfo,score);
+			if(e.getErrorCode() == 2003 || e.getErrorCode() == 2010){//gapped seq||no sequnce name
+				Sequence seq = new FastQSequence(header, sequence,addInfo,score);
 				seqList.add(seq);
 				errList.add(e);
 			}else{
-			Sequence seq = new Sequence(null,null);
+			Sequence seq = new FastQSequence(null,null,null,null);
 			seqList.add(seq);
 			errList.add(e);
 			}
 		}
 	}
 	
-	private void checkForReadingError(){
+	private void checkForReadingError(String identity, String sequence, String addInfo, String score) throws MyException{
+		
+		char[] id = identity.toCharArray();
+		boolean emptyName = true;
+		for (int i = 1; i < id.length; i++) {
+			if (id[i] != ' ') {
+				emptyName = false;
+				break;
+			}
+		}
+		if(emptyName){
+			throw new MyException(ErrorCodes.NO_SEQUENCE_NAME);
+		}
+		
+		if(sequence.isEmpty()){
+			throw new MyException(ErrorCodes.NO_SEQUENCE);
+		}
+		
+		if(sequence.contains("-")){
+			throw new MyException(ErrorCodes.GAPPED_SEQUENCE);
+		}
+		
+		for (int i = 0; i < sequence.length(); i++) {
+			if ((!sequence.contains("A") && !sequence.contains("C")
+					&& !sequence.contains("T") && !sequence.contains("G"))
+					&& (!sequence.contains("a") && !sequence.contains("c")
+							&& !sequence.contains("t") && !sequence
+								.contains("g"))) {
+				throw new MyException(ErrorCodes.CORRUPTED_SEQUENCE);
+			}
+		}
+		
+		
+		if (sequence != sequence.toUpperCase()) {
+			throw new MyException(ErrorCodes.LOWERCASE_SEQUENCE);
+		}
+		
+		if(!(score.length() == sequence.length())){
+			throw new MyException(ErrorCodes.INCORRECT_SEQUENCE_SCORE);
+		}
+		
+		
 	}
 
-	public void writeInFile(String filename) {
-		// TODO Auto-generated method stub
-		
+	public void writeInFile(String outputFilename) {
+		File writeInFile = new File(outputFilename);
+		try {
+			new FileOutputStream(writeInFile,true);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+			try
+			{
+				FileWriter fw = new FileWriter("./" + writeInFile,true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				for (int i = 0; i < seqList.size();i++) {
+					bw.append(seqList.get(i).getHeader());
+					bw.newLine();
+					bw.append(seqList.get(i).getSequence());
+					bw.newLine();
+					bw.append(((FastQSequence) seqList.get(i)).getAddInfo());
+					bw.newLine();
+					bw.append(seqList.get(i).getScore());
+					bw.newLine();
+					bw.newLine();
+				}
+
+
+				bw.close();
+				fw.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+
+//			//TODO check if user should be informed about the fact that the filename already exists, so far new content gets appended to old one
+
 	}
 
 	public void addSeq(Sequence seq) throws MyException {
 		// TODO Auto-generated method stub
-		
+		processRead(seq.getHeader(),seq.getSequence(),((FastQSequence) seq).getAddInfo(), seq.getScore());
 	}
 
 	public ArrayList<Sequence> getSequence() {
 		// TODO Auto-generated method stub
-		return null;
+		return seqList;
 	}
 
 	public ArrayList<MyException> getErrorInSequence() {
 		// TODO Auto-generated method stub
-		return null;
+		return errList;
 	}
 	
-
+//public static void main(String[] args) throws IOException, MyException{
+//	FiletypeContainingSequences fastQ = new FastQ();
+//	Sequence seq = new FastQSequence("me","ACTG","+ a FastQ test", "$%^&");
+//	fastQ.addSeq(seq);
+//	fastQ.parse("C:/Users/Friederike/University/Fourth Semester/Programmierprojekt/git/MinION2015/Minion/src/FastQTestFile.fastq");
+//	fastQ.writeInFile("TestFastQWirteInfile.txt");
+//}
 }
 
 
-//public class FastQParser {
-//
-//	private BufferedReader bufferReader;
-//	private ArrayList<FastQEntry> fastQList;
-//
-//	/**
-//	 * Gets a file name and creates a new FastQPArser object just for this file. The file is then processed. 
-//	 * @param inputFile
-//	 * @throws IOException
-//	 */
-//	public FastQParser(String inputFile) throws IOException {
-//		// so it's not necessary to type in the file ending, just the name
-//		inputFile += ".fastq";
-//		File file = new File(inputFile);
-//		FileReader fileReader = new FileReader(file);
-//		this.bufferReader = new BufferedReader(fileReader);
-//		this.fastQList = new ArrayList<FastQEntry>();
-//
-//		run();
-//
-//	}
-//
-//	private void run() throws IOException {
-//		processFile();
-//		printFile();
-//	}
-//
-//	private void printFile() {
-//		System.out.println("We found the following sequences: ");
-//		for (FastQEntry entry : fastQList) {
-//			System.out.println(entry.getIdentity() + "\n" + entry.getSequence() + "\n"
-//					+ entry.getAddInfo() + "\n" + entry.getScore());
-//
-//		}
-//	}
-//
-//	/**
-//	 * Each line is processed. Lines staring with an '@', as long as they can't be a scoring line are stored in identity, same for sequences(only upper case letters, additional Information('+') and the score.
-//	 * If a line has '@' as it's first letter and the score is not read everything gets reset(since it can't be a correctly displayed sequence) and it is retried to store the following potential sequence.
-//	 * In case the scoring line starts with an '@' but is in fact the next sequence header, so far we can only check if the lengths of the sequence and the header are differing from each other.
-//	 * Worst case scenario would be that they share the same length although the header is not the scoring line. This would cause the next sequence if it is displayed correctly to be thrown out.
-//	 * This seems fairly unlikely but it should be caught regardless. One possibility would be to check specifically for Illuminasince they have further requirements for the header.
-//	 * @throws IOException
-//	 */
-//
-//	private void storeSequence(String sequence, String identity,
-//			String addInfo, String score) {
-//		FastQEntry fQEntry = new FastQEntry(identity, sequence, addInfo, score);
-//		fastQList.add(fQEntry);
-//
-//	}
-//
-//	private boolean entryCorrect(String sequence, String identity,
-//			String addInfo, String score) {
-//		if (!(sequence.isEmpty() || identity.isEmpty() || addInfo.isEmpty() || score
-//				.isEmpty()) && sequence.length() == score.length()) {
-//			return true;
-//		}
-//		return false;
-//	}
-//
-//	public ArrayList<FastQEntry> getFastQList() {
-//		return fastQList;
-//	}
-//
-//	public void setFastQList(ArrayList<FastQEntry> fastQList) {
-//		this.fastQList = fastQList;
-	}
+	
